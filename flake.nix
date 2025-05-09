9cdf47ed10717a32c0f17c9532bb59549317e41b
andy256789 <anonovatanq3@gmail.com>
2025-05-09 12:06:10 +0300

    testing new nix config

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
 	description = "Andy's NixOS configuration";
-	nixConfig = {
-		extra-substituters = [
-			"https://hyprland.cachix.org/"
-			"https://cache.nixos.org/"
-		];
-		extra-trusted-public-keys = [
-			"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-			"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-		];
-	};
+  # https://discourse.nixos.org/t/how-to-set-up-cachix-in-flake-based-nixos-config/31781
+  # use --accept-flake-config when rebuilding
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
--- hosts/andy-desktop/default.nix
+++ hosts/andy-desktop/default.nix
@@ -13,18 +13,19 @@
   virtualisation.vmware.guest.enable = true;
 
   # Nix settings
-  nix.settings = {
-     experimental-features = [ "nix-command" "flakes" ];
-     substituters = [
-       "https://hyprland.cachix.org/"
-       "https://cache.nixos.org/"
-     ];
-     trusted-public-keys = [
-       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-     ];
-  };
   nixpkgs.config.allowUnfree = true;
+  # nix.settings = {
+  #    experimental-features = [ "nix-command" "flakes" ];
+  #    substituters = [
+  #      "https://hyprland.cachix.org/"
+  #      "https://cache.nixos.org/"
+  #    ];
+  #    trusted-public-keys = [
+  #      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+  #      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+  #    ];
+  # };
+
 
   # System settings
   networking.hostName = "andy-desktop";
