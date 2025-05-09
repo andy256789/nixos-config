3545af921a4a7362907580a16e4304b0e8eb81df
andy256789 <andonovatanq3@gmail.com>
2025-05-09 11:57:44 +0300

    updating substituters for nix config

--- flake.nix
+++ flake.nix
@@ -1,44 +1,54 @@
 {
-  description = "Andy's NixOS configuration";
+	description = "Andy's NixOS configuration";
+	nixConfig = {
+		extra-substituters = [
+			"https://hyprland.cachix.org/"
+			"https://cache.nixos.org/"
+		];
+		extra-trusted-public-keys = [
+			"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+			"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+		];
+	};
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hyprland = {
-      url = "github:hyprwm/Hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		hyprland = {
+			url = "github:hyprwm/Hyprland";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
 
-  outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs:
-    let
-      system = "x86_64-linux";
-    in {
-      nixosConfigurations = {
-        andy-desktop = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/andy-desktop
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {
-                  inherit inputs;
-                  username = "andy";
-                 };
-              home-manager.users.andy = import ./hosts/andy-desktop/home.nix;
-            }
-          ];
-          specialArgs = {
-            inherit self inputs hyprland;
-            username = "andy";
-            host = "andy-desktop";
-          };
-        };
-      };
-    };
+	outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs:
+		let
+		system = "x86_64-linux";
+	in {
+		nixosConfigurations = {
+			andy-desktop = nixpkgs.lib.nixosSystem {
+				inherit system;
+				modules = [
+					./hosts/andy-desktop
+						home-manager.nixosModules.home-manager
+						{
+							home-manager.useGlobalPkgs = true;
+							home-manager.useUserPackages = true;
+							home-manager.extraSpecialArgs = {
+								inherit inputs;
+								username = "andy";
+							};
+							home-manager.users.andy = import ./hosts/andy-desktop/home.nix;
+						}
+				];
+				specialArgs = {
+					inherit self inputs hyprland;
+					username = "andy";
+					host = "andy-desktop";
+				};
+			};
+		};
+	};
 }
