31feb9bebfc77bdb79c99d7a3c8c69cfe2976b89
andy256789 <andonovatanq3@gmail.com>
2025-05-12 08:54:44 +0300

    updating

--- hosts/andy-desktop/default.nix
+++ hosts/andy-desktop/default.nix
@@ -1,92 +1,118 @@
 { config, pkgs, hyprland, settings, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+	imports = [
+		./hardware-configuration.nix
+	];
 
-  # Bootloader
-  boot.loader.systemd-boot.enable = false;
-  
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub = {
-       enable = true;
-       efiSupport = true;
-       useOSProber = true;
-       devices = [ "nodev" ];
-  };
-       
+# Bootloader
+	boot.loader.systemd-boot.enable = false;
 
-  #virtualisation.vmware.guest.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+	boot.loader.grub = {
+		enable = true;
+		efiSupport = true;
+		useOSProber = true;
+		devices = [ "nodev" ];
+	};
 
-  # Nix settings
-  nixpkgs.config.allowUnfree = true;
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-  };
 
-  # System settings
-  networking.hostName = settings.hostname;
-  time.timeZone = "Europe/Sofia";
+#virtualisation.vmware.guest.enable = true;
 
-  # User configuration
-  users.users.${settings.username} = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "video" "input" ];
-    shell = pkgs.fish;
-  };
+# Nix settings
+	nixpkgs.config.allowUnfree = true;
+	nix.settings = {
+		experimental-features = [ "nix-command" "flakes" ];
+	};
 
-  # Networking
-  networking.networkmanager.enable = true;
+# System settings
+	networking.hostName = settings.hostname;
+	time.timeZone = "Europe/Sofia";
+
+# User configuration
+	users.users.${settings.username} = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" "networkmanager" "video" "input" ];
+		shell = pkgs.fish;
+	};
+
+# Networking
+	networking.networkmanager.enable = true;
 
 # Display and window manager
-  services.xserver.enable = true;
-  services.xserver.extraConfig = ''
-	  Section "Monitor"
-	  Identifier "DP-2"
-	  Modeline "2560x1440_170.00" 974.00 2560 2792 3080 3600 1440 1443 1448 1592 -hsync +vsync
-	  Option "PreferredMode" "2560x1440_170.00"
-	  EndSection
-	  '';
+	services.xserver.enable = true;
 
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
-  programs.hyprland = {
-    enable = true;
-    package = pkgs.hyprland;
-    xwayland.enable = true;
-  };
+# AMD GPU Boost & 170Hz Fix
+	services.xserver.extraConfig = ''
+		Section "Monitor"
+		Identifier "DP-2"
+		Modeline "2560x1440_170.00" 974.00 2560 2792 3080 3600 1440 1443 1448 1592 -hsync +vsync
+		Option "PreferredMode" "2560x1440_170.00"
+		EndSection
 
-  # Enable OpenGL and Mesa
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [ mesa ];
-  };
+		Section "Device"
+		Identifier "AMD"
+		Driver "amdgpu"
+		Option "TearFree" "true"
+		Option "VariableRefresh" "true"
+		Option "DRI" "3"
+		EndSection
+		'';
 
-  # System packages (only system-level packages that shouldn't be in home-manager)
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-    wget
-    mesa
-    wayland-utils
-  ];
+# AMD kernel module options
+	boot.kernelModules = [ "amdgpu" ];
 
-  # Shell
-  programs.fish.enable = true;
-  services.sshd.enable = true;
+	boot.extraModprobeConfig = ''
+		options amdgpu dc=1
+		options amdgpu vrr_support=1
+		options amdgpu si_support=0
+		options amdgpu cik_support=0
+		'';
 
-  # Environment variables
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1"; # Enable Wayland for Ozone-based apps (e.g., Firefox)
-    WLR_NO_HARDWARE_CURSORS = "1"; # Fix cursor issues in some VMs
-    WLR_RENDERER_ALLOW_SOFTWARE = "1";
-    QT_QPA_PLATFORM = "wayland"; # Force Qt apps to use Wayland
-    GDK_BACKEND = "wayland"; # Force GTK apps to use Wayland
-  };
+# Force performance governor (optional)
+	services.udev.extraRules = ''
+		SUBSYSTEM=="drm", ACTION=="change", RUN+="${pkgs.util-linux}/bin/echo performance > /sys/class/drm/card0/device/power_dpm_force_performance_level"
+		'';
 
-  # System state version
-  system.stateVersion = "24.11";
+
+	services.displayManager.sddm = {
+		enable = true;
+		wayland.enable = true;
+	};
+	programs.hyprland = {
+		enable = true;
+		package = pkgs.hyprland;
+		xwayland.enable = true;
+	};
+
+# Enable OpenGL and Mesa
+	hardware.graphics = {
+		enable = true;
+		extraPackages = with pkgs; [ mesa ];
+	};
+
+# System packages (only system-level packages that shouldn't be in home-manager)
+	environment.systemPackages = with pkgs; [
+		git
+			vim
+			wget
+			mesa
+			wayland-utils
+	];
+
+# Shell
+	programs.fish.enable = true;
+	services.sshd.enable = true;
+
+# Environment variables
+	environment.sessionVariables = {
+		NIXOS_OZONE_WL = "1"; # Enable Wayland for Ozone-based apps (e.g., Firefox)
+			WLR_NO_HARDWARE_CURSORS = "1"; # Fix cursor issues in some VMs
+			WLR_RENDERER_ALLOW_SOFTWARE = "1";
+		QT_QPA_PLATFORM = "wayland"; # Force Qt apps to use Wayland
+			GDK_BACKEND = "wayland"; # Force GTK apps to use Wayland
+	};
+
+# System state version
+	system.stateVersion = "24.11";
 }
