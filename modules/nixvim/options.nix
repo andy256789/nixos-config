4d68dc6c883789f83483533b9ca90a548ffd4249
andy256789 <andonovatanq3@gmail.com>
2025-05-13 16:38:52 +0300

    added c development tools]

--- modules/nixvim/options.nix
+++ modules/nixvim/options.nix
@@ -1,75 +1,80 @@
 {
-  config.programs.nixvim = {
-    # Disable netrw banner
-    globals = {
-      netrw_banner = 0;
-      editorconfig = true;
+    config.programs.nixvim = {
+        # Disable netrw banner
+        globals = {
+            netrw_banner = 0;
+            editorconfig = true;
+        };
+
+        # General cursor and display settings
+        opts = {
+            # Cursor
+            guicursor = "a:block";
+
+            # Numbers
+            number = true;
+            relativenumber = true;
+
+            # Indentation
+            tabstop = 4;
+            softtabstop = 4;
+            shiftwidth = 4;
+            expandtab = true;
+            autoindent = true;
+            smartindent = true;
+            wrap = false;
+
+            # Files
+            swapfile = false;
+            backup = false;
+            undofile = true;
+            undodir = "$HOME/.vim/undodir";
+
+            # Search
+            incsearch = true;
+            inccommand = "split";
+            ignorecase = true;
+            smartcase = true;
+            hlsearch = true;
+
+            # Colors
+            termguicolors = true;
+            background = "dark";
+            colorcolumn = "80";
+
+            # UI
+            scrolloff = 8;
+            signcolumn = "yes";
+
+            # Folding
+            foldenable = true;
+            foldmethod = "manual";
+            foldlevel = 99;
+            foldcolumn = "0";
+
+            # Backspace behavior
+            backspace = ["start" "eol" "indent"];
+
+            # Window splitting
+            splitright = true;
+            splitbelow = true;
+
+            # File path
+            isfname.append = "@-@";
+
+            # Performance
+            updatetime = 50;
+
+            # Clipboard
+            clipboard = {
+                providers = {
+                    wl-copy.enable = true; # Wayland 
+                    xsel.enable = true; # For X11
+                };
+                register = "unnamedplus";
+            };
+            # Mouse
+            mouse = "a";
+        };
     };
-
-    # General cursor and display settings
-    opts = {
-      # Cursor
-      guicursor = "";
-
-      # Numbers
-      number = true;
-      relativenumber = true;
-
-      # Indentation
-      tabstop = 4;
-      softtabstop = 4;
-      shiftwidth = 4;
-      expandtab = true;
-      autoindent = true;
-      smartindent = true;
-      wrap = false;
-
-      # Files
-      swapfile = false;
-      backup = false;
-      undofile = true;
-      undodir = "$HOME/.vim/undodir";
-
-      # Search
-      incsearch = true;
-      inccommand = "split";
-      ignorecase = true;
-      smartcase = true;
-      hlsearch = true;
-
-      # Colors
-      termguicolors = true;
-      background = "dark";
-      colorcolumn = "80";
-
-      # UI
-      scrolloff = 8;
-      signcolumn = "yes";
-
-      # Folding
-      foldenable = true;
-      foldmethod = "manual";
-      foldlevel = 99;
-      foldcolumn = "0";
-
-      # Backspace behavior
-      backspace = ["start" "eol" "indent"];
-
-      # Window splitting
-      splitright = true;
-      splitbelow = true;
-
-      # File path
-      isfname.append = "@-@";
-      
-      # Performance
-      updatetime = 50;
-
-      # Clipboard
-      clipboard.append = "unnamedplus";
-
-      # Mouse
-      mouse = "a";
-    };
-  };
 } 
