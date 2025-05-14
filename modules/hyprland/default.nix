af9e882b7d18a7fd79b0091119ea4f3175c6f0a8
andy256789 <andonovatanq3@gmail.com>
2025-05-14 06:49:05 +0300

    updated monitor scale

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -36,7 +36,7 @@ in {
 
         monitors = mkOption {
             type = types.listOf types.str;
-            default = ["DP-2,2560x1440@169.83,0x0,1.2"];
+            default = ["DP-2,2560x1440@169.83,0x0,1.5"];
             description = "Monitor configuration";
         };
     };
