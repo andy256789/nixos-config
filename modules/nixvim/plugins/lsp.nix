bc195d28641280ec8b4dc775d4073e7c3bdf04c4
andy256789 <andonovatanq3@gmail.com>
2025-07-15 18:32:31 +0300

    added c to nixvim lsp fixing errors

--- modules/nixvim/plugins/lsp.nix
+++ modules/nixvim/plugins/lsp.nix
@@ -46,5 +46,55 @@
         };
 
         lsp-format.enable = true;
+
+        # Autocompletion setup
+        nvim-cmp = {
+            enable = true;
+            autoEnableSources = true;
+            
+            sources = [
+                { name = "nvim_lsp"; }
+                { name = "path"; }
+                { name = "buffer"; }
+                { name = "luasnip"; }
+            ];
+
+            formatting = {
+                format = ''
+                    function(entry, vim_item)
+                        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+                        vim_item.menu = ({
+                            buffer = "[Buffer]",
+                            nvim_lsp = "[LSP]",
+                            luasnip = "[LuaSnip]",
+                            path = "[Path]",
+                        })[entry.source.name]
+                        return vim_item
+                    end
+                '';
+            };
+
+            mapping = {
+                "<C-k>" = "cmp.mapping.select_prev_item()";
+                "<C-j>" = "cmp.mapping.select_next_item()";
+                "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+                "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                "<C-Space>" = "cmp.mapping.complete()";
+                "<C-e>" = "cmp.mapping.abort()";
+                "<CR>" = "cmp.mapping.confirm({ select = true })";
+                "<Tab>" = "cmp.mapping(function(fallback) if cmp.visible() then cmp.select_next_item() else fallback() end end, {'i', 's'})";
+                "<S-Tab>" = "cmp.mapping(function(fallback) if cmp.visible() then cmp.select_prev_item() else fallback() end end, {'i', 's'})";
+            };
+        };
+
+        # Completion sources
+        cmp-nvim-lsp.enable = true;
+        cmp-buffer.enable = true;
+        cmp-path.enable = true;
+        cmp_luasnip.enable = true;
+
+        # Snippet engine (required for nvim-cmp)
+        luasnip.enable = true;
+        friendly-snippets.enable = true;
     };
 } 
